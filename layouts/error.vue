98fa7afdd85958afbd215384e9aba1aba5425de2
nittashiori <nitta@impv.co.jp>
2020-02-25 12:06:28 +0900

    :+1: エラーページを追加

---
+++ layouts/error.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <Header />
+    <section class="container">
+      <PageTitle>
+        {{ title }}
+      </PageTitle>
+      <p class="text">
+        申し訳ありません。<br>
+        お探しのページは削除された可能性があります。
+      </p>
+      <div class="btn-wrap">
+        <nuxt-link to="/" class="btn btn--center">
+          ホームに戻る
+        </nuxt-link>
+      </div>
+    </section>
+    <Footer />
+    <GlobalNavSp />
+  </div>
+</template>
+
+<script>
+import PageTitle from '~/components/PageTitle.vue'
+export default {
+  components: {
+    PageTitle
+  },
+  data () {
+    return {
+      title: 'ページが見つかりませんでした'
+    }
+  }
+}
+</script>
